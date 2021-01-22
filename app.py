6496ab117cd4f335481e241e9f1bdd334dbd8fd0
amit <amitjain2110@gmail.com>
2021-01-22 12:36:54 +0530

    Initial commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) # used in pycharm + flask browser run the code
 
 
 # @cross_origin()
